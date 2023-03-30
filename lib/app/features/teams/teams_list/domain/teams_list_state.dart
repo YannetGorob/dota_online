11c8460af2bcd433a6ff9707f948bb50eaca933a
Yana Horobchuk <78377338+YannetGorob@users.noreply.github.com>
2023-03-30 17:44:54 +0300

    fix: di.dart

---
+++ lib/app/features/teams/teams_list/domain/teams_list_state.dart
@@ -0,0 +1,11 @@
+import 'package:dota_online/core/api/responses/teams/teams_response.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'teams_list_state.freezed.dart';
+
+@freezed
+class TeamsListState with _$TeamsListState {
+  const factory TeamsListState.loading() = Loading;
+  const factory TeamsListState.loaded({required List<Teams> teams,}) = Loaded;
+  const factory TeamsListState.error([String? message]) = Error;
+}
