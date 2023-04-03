d4702606777f70c503a92c7ff59cfdedc7617335
Dmytro Tkachuk <124440580+RED-offline@users.noreply.github.com>
2023-04-03 13:57:56 +0300

    fix: delete Hero model

--- lib/core/api/providers/heroes_provider.dart
+++ lib/core/api/providers/heroes_provider.dart
@@ -1,22 +1,11 @@
 import 'package:dota_online/core/api/base/api_result.dart';
 import 'package:dota_online/core/api/base/base_network.dart';
-import 'package:dota_online/core/api/models/hero/hero_model.dart';
 import 'package:dota_online/core/api/models/hero/hero_stats.dart';
 
 class HeroesProvider {
   HeroesProvider(this._network);
 
   final BaseNetwork _network;
-
-  Future<ApiResult<List<HeroModel>>> getHeroes() {
-    return _network.sendRequest<List<HeroModel>>(
-      path: '/heroes',
-      parseListResponse: (json) {
-        return List<HeroModel>.from(
-            json.map((e) => HeroModel.fromJson(e as Map<String, dynamic>)));
-      },
-    );
-  }
 
   Future<ApiResult<List<HeroStats>>> getHeroStats() {
     return _network.sendRequest<List<HeroStats>>(
