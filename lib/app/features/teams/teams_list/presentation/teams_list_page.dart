e2cabdd31bf83b4bb130a6b7004d011383fad773
Yehor Pustovoit <63584ee@gmail.com>
2023-04-03 10:40:54 +0300

    feat: add basic teams list ui

--- lib/app/features/teams/teams_list/presentation/teams_list_page.dart
+++ lib/app/features/teams/teams_list/presentation/teams_list_page.dart
@@ -3,29 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:dota_online/core/api/models/teams/teams.dart';
 
 class TeamsListPage extends StatelessWidget {
-  TeamsListPage({
-    Key? key,
-    required this.detailsPath,
-    /* required this.teams*/
-  }) : super(key: key);
+  TeamsListPage({Key? key, required this.detailsPath,}) : super(key: key);
+
   final String detailsPath;
 
   late final List<Teams> teams;
 
   @override
   Widget build(BuildContext context) {
-    teams = List.generate(10, (index) {
-      return Teams(
-        name: '$index team',
-        logoUrl: index.isOdd
-            ? null
-            : 'https://liquipedia.net/commons/images/thumb/f/fd/Team_Random_logo.png/600px-Team_Random_logo.png',
-       // losses: 4,
-        rating: 1,
-        wins: 7,
-        lastMatchTime: 162983620234
-      );
-    });
     return TeamsListWidget(detailsPath: detailsPath, teams: teams);
   }
 }
--- lib/app/features/teams/teams_list/presentation/widgets/teams_list_item.dart
+++ lib/app/features/teams/teams_list/presentation/widgets/teams_list_item.dart
@@ -5,30 +5,35 @@ class TeamsListItem extends StatelessWidget {
   TeamsListItem({required this.team});
 
   final Teams team;
-  final defaultImage =
+  final  defaultImage =
       'https://t4.ftcdn.net/jpg/04/99/93/31/360_F_499933117_ZAUBfv3P1HEOsZDrnkbNCt4jc3AodArl.jpg';
 
   @override
   Widget build(BuildContext context) {
-
-    return team.name == null
-        ? Container()
-        : Card(
+    final canShowTeam = team.name != null && team.rating != null;
+    return canShowTeam
+        ? Card(
             color: Colors.black38,
             elevation: 5,
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 60,
-                    backgroundColor: Colors.black54,
-                    backgroundImage: NetworkImage(team.logoUrl ?? defaultImage),
-                  ),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.black54,
+                  backgroundImage: NetworkImage(team.logoUrl ?? defaultImage),
+                ),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                   Text('team.name: ' + team.name!),
                   Text('team.rating: ' + team.rating.toString()),
-                  Text('team.wins: ' + team.wins.toString()),
-                  Text('team.losses: ' + team.losses.toString())
+                  if (team.wins != null)
+                    Text('team.wins: ' + team.wins.toString()),
+                  if (team.losses != null)
+                    Text('team.losses: ' + team.losses.toString())
                 ]),
-          );
+              ],
+            ),
+          )
+        : Container();
   }
 }
