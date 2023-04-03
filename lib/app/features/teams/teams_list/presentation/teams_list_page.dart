7d29b11c10154fe82ff6544ca4f2dae182be6d30
Yehor Pustovoit <63584ee@gmail.com>
2023-04-03 17:01:29 +0300

    fix: fix teams list page

--- lib/app/features/teams/teams_list/presentation/teams_list_page.dart
+++ lib/app/features/teams/teams_list/presentation/teams_list_page.dart
@@ -1,6 +1,10 @@
+import 'package:dota_online/app/features/teams/teams_list/domain/teams_list_cubit.dart';
 import 'package:dota_online/app/features/teams/teams_list/presentation/widgets/teams_list_widget.dart';
+import 'package:dota_online/core/api/dota_api.dart';
+import 'package:dota_online/core/di/di.dart';
 import 'package:flutter/material.dart';
 import 'package:dota_online/core/api/models/team/team_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TeamsListPage extends StatelessWidget {
   TeamsListPage({Key? key, required this.detailsPath}) : super(key: key);
@@ -10,6 +14,8 @@ class TeamsListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TeamsListWidget(detailsPath: detailsPath, teams: teams);
+    return BlocProvider<TeamsListCubit>(
+      create: (_) => TeamsListCubit(teamsProvider: locator.get<DotaApi>().teams),
+    );
   }
 }
