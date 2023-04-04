32f358aea2e4d9f0b97eecb12cd5d4a349d8cfc6
Yana Horobchuk <78377338+YannetGorob@users.noreply.github.com>
2023-04-04 10:21:40 +0300

    feat: register navigator

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -1,9 +1,10 @@
 import 'package:dota_online/core/api/dota_api.dart';
+import 'package:dota_online/core/navigation/navigation.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.I;
 
 void configureDependencies() {
   locator.registerLazySingleton<DotaApi>(() => DotaApi.init());
-  //router
+  locator.registerLazySingleton<Navigation>(() => Navigation());
 }
