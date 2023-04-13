59ecb288dbf7e318f270b09ce3a8a30dffb5b9b2
Yana Horobchuk <78377338+YannetGorob@users.noreply.github.com>
2023-04-13 11:49:27 +0300

    fix: add getPlayerDetails & getPlayerRecentMatches

--- lib/core/api/providers/players_provider.dart
+++ lib/core/api/providers/players_provider.dart
@@ -1,5 +1,7 @@
 import 'package:dota_online/core/api/base/api_result.dart';
 import 'package:dota_online/core/api/base/base_network.dart';
+import 'package:dota_online/core/api/models/player/player_details_model.dart';
+import 'package:dota_online/core/api/models/player/player_recent_matches_model.dart';
 import 'package:dota_online/core/api/models/player/pro_player_model.dart';
 import 'package:dota_online/core/api/models/player/search_player_model.dart';
 
@@ -22,15 +24,38 @@ class PlayersProvider {
   Future<ApiResult<List<SearchPlayerModel>>> searchPlayersByName(
       String personName) {
     return _network.sendRequest(
-        path: '/search',
-        queryParameters: {
-          'q': personName,
-        },
-        parseListResponse: (json) {
-          return List<SearchPlayerModel>.from(
-            json.map(
-                (e) => SearchPlayerModel.fromJson(e as Map<String, dynamic>)),
-          );
-        });
+      path: '/search',
+      queryParameters: {
+        'q': personName,
+      },
+      parseListResponse: (json) {
+        return List<SearchPlayerModel>.from(
+          json.map(
+              (e) => SearchPlayerModel.fromJson(e as Map<String, dynamic>)),
+        );
+      },
+    );
+  }
+
+  Future<ApiResult<PlayerDetailsModel>> getPlayerDetails(int accountId) {
+    return _network.sendRequest(
+      path: '/players/$accountId',
+      parseResponse: (json) {
+        return PlayerDetailsModel.fromJson(json);
+      },
+    );
+  }
+
+  Future<ApiResult<List<PlayerRecentMatchesModel>>> getPlayerRecentMatches(
+      int accountId) {
+    return _network.sendRequest(
+      path: '/players/$accountId/recentMatches',
+      parseListResponse: (json) {
+        return List<PlayerRecentMatchesModel>.from(
+          json.map((e) =>
+              PlayerRecentMatchesModel.fromJson(e as Map<String, dynamic>)),
+        );
+      },
+    );
   }
 }
